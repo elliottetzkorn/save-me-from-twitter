a1dd255d118246b23fe3ba180874ba06d1fc2c96
Elliott Etzkorn <eetzkorn@icloud.com>
2022-04-27 17:57:02 -0700

    Init commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<div class="container mx-auto ">
+	<img alt="heaven" class="w-full md:px-4" src="/field.jpg" />
+	<div class="flex flex-row mt-4 px-4">
+		<div class="flex flex-auto">
+			<p class="text-2xl">Twitter can be better</p>
+		</div>
+		<div class="flex flex-none">
+			<p class="text-l font-bold  hover:text-blue-500 hover:cursor-pointer hover:underline">
+				Log in →
+			</p>
+		</div>
+	</div>
+</div>
